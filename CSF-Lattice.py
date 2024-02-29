d1f72c5dc65b0938b6ea134dd161d7dfef5af21c
Sina Ghaseminejad <sina.qane@yahoo.com>
2024-02-29 14:49:43 +0330

    Added NEW ALE/CLE functions

--- CSF-Lattice.py
+++ CSF-Lattice.py
@@ -167,15 +167,83 @@ def find_level_equal_same_size(list_of_partition1, list_of_partition2, constrain
     #     if partition_comparer(partition, selected_level_equal) == constraint:
     #         selected_level_equal = partition
     # return selected_level_equal
+    
+def new_find_ancestor_level_equal(partition1, partition2):
+    return new_find_ancestor_level_equal_util([make_standard_partiion(partition1)], [make_standard_partiion(partition2)])
 
-a = [4, 4]
-b = [5, 3]
+def new_find_child_level_equal(partition1, partition2):
+    return new_find_child_level_equal_util([make_standard_partiion(partition1)], [make_standard_partiion(partition2)])
+
+def new_find_ancestor_level_equal_util(list_of_partition1, list_of_partition2):
+    if len(list_of_partition1[0]) < len(list_of_partition2[0]) :
+        return new_find_ancestor_level_equal_util(list_of_partition2, list_of_partition1)
+    all_partitions = partitions(sum(list_of_partition1[0]))
+    while len(list_of_partition1[0]) > len(list_of_partition2[0]):
+        list_of_partition1 = shrinker_list(list_of_partition1)
+        list_of_partition1 = complete_list(list_of_partition1, all_partitions)
+    return find_level_equal_same_size(list_of_partition1, list_of_partition2, -1)
+
+def new_find_child_level_equal_util(list_of_partition1, list_of_partition2):
+    if len(list_of_partition1[0]) > len(list_of_partition2[0]) :
+        return new_find_child_level_equal_util(list_of_partition2, list_of_partition1)
+    all_partitions = partitions(sum(list_of_partition1[0]))
+    while len(list_of_partition1[0]) < len(list_of_partition2[0]):
+        list_of_partition1 = expander_list(list_of_partition1)
+        list_of_partition1 = complete_list(list_of_partition1, all_partitions)
+    return find_level_equal_same_size(list_of_partition1, list_of_partition2, +1)
+
+def complete_list(list_of_partition1, all_partitions):
+    min_partition = make_standard_partiion(sort_partitions(list_of_partition1)[0])
+    same_size_partitions = [partition for partition in all_partitions if len(partition) == len(min_partition)]
+    same_size_partitions = list(reversed(sort_partitions(same_size_partitions)))
+
+    index = 0
+    for i in range(0, len(same_size_partitions)):
+        same_size_partitions[i] = make_standard_partiion(same_size_partitions[i])
+        if same_size_partitions[i] == min_partition:
+            index = i
+    
+    return same_size_partitions[index:]
+
+def partitions(number):
+    part = partition_maker(number)
+    partition = list(part)
+    for i in range(len(partition)):
+        partition[i] = list(partition[i])
+    final = []
+    for i in range(number):
+        final.append([])
+    for part in partition:
+        final[len(part) - 1].append(part)
+    ans = []
+    for parts in final:
+        temp = sorted(parts, reverse=True)
+        for par in temp:
+            ans.append(par)
+    return ans
+
+def partition_maker(number):
+    answer = set()
+    answer.add((number,))
+    for x in range(1, number):
+        for y in partition_maker(number - x):
+            answer.add(tuple(sorted((x,) + y, reverse=True)))
+    return answer
+
+a = [4, 3, 1]
+b = [6, 2]
 
 print("ANCESTOR LEVEL EQUAL:")
 print(find_ancestor_level_equal(a, b))
 
 print("\nCHILD LEVEL EQUAL:")
 print(find_child_level_equal(a, b))
+
+print("\nNEW ANCESTOR LEVEL EQUAL:")
+print(new_find_ancestor_level_equal(a, b))
+
+print("\nNEW CHILD LEVEL EQUAL:")
+print(new_find_child_level_equal(a, b))
 
 print("\nHIGHEST COMMON CHILD:")
 print(find_highest_common_child(a, b))
